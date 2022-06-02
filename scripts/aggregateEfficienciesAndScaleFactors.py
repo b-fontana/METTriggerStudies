ab3cbeb628fa757666398575a0446afef55cc2bc
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-06-02 15:47:03 +0200

    :sparkles: conversion from TGraphAsymmErrors to TH1D

--- scripts/aggregateEfficienciesAndScaleFactors.py
+++ scripts/aggregateEfficienciesAndScaleFactors.py
@@ -19,6 +19,7 @@ from ROOT import (
     TLine,
     TPad,
     )
+from array import array
 
 import sys
 sys.path.append( os.path.join(os.environ['CMSSW_BASE'], 'src', 'METTriggerStudies'))
@@ -39,6 +40,35 @@ from luigi_conf import (
     _extensions,
     _placeholder_cuts,
     )
+
+def convertGraphToHist(graph):
+    """
+    Converts 1D TGraphAsymmErrors to TH1D.
+    TODO: support 2D graphs [3D histograms hopefully not...]
+    The convertion is required due to ROOT's bad support of 2D objects:
+    - TGraph2DAsymmErrors must have the same number of points in X and Y!
+    - TGraph3DAsymmErrors does not exist
+    - Histograms are the most flexible, although do not support asym errors.
+    """
+    n = graph.GetN()
+    name = graph.GetName()
+
+    # define x (bin centers)
+    x = array('d')
+    x.append((3.0 * graph.GetX()[0] - graph.GetX()[1]) / 2.0)
+    for i in range(1,n):
+        x.append((graph.GetX()[i-1] + graph.GetX()[i]) / 2.0)
+    x.append((3.0 * graph.GetX()[n-1] - graph.GetX()[n-2]) / 2.0)
+    histo = ROOT.TH1D(name, name, n, x)
+
+    # fill y values (counts)
+    for i in range(n):
+        histo.SetBinContent(i+1, graph.GetY()[i])
+        # losing up/down asymmetric info...
+        toterr = graph.GetErrorYhigh(i)+graph.GetErrorYlow(i)
+        histo.SetBinError(i+1, toterr)
+
+    return histo
 
 def aggregateEfficienciesAndScaleFactors(indir, outdir, channel, subtag, prefix, variables, debug):
     extension = 'root'
@@ -63,7 +93,8 @@ def aggregateEfficienciesAndScaleFactors(indir, outdir, channel, subtag, prefix,
                     if not cl.InheritsFrom('TGraph'):
                         continue
                     h = key.ReadObj()
-
+                    h = convertGraphToHist(h)
+                    
                     var, trigger, cut = regex.match(afile).groups()
                     new_name = h.GetName() + '_VAR_' + var + '_TRG_' + trigger + '_CUT_' + cut
 
