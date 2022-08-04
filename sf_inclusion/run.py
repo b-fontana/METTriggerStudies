1e48640c607c09a2fa6e2835909b6cc15144c4e7
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-08-04 15:26:27 +0200

    change dag queue to the maximum (long)

--- sf_inclusion/run.py
+++ sf_inclusion/run.py
@@ -376,7 +376,7 @@ class SubmitDAG(lutils.ForceRun):
         with open(out, 'r') as f:
             contents = f.readlines()
         ncontents = len(contents)
-        new_content = jw.condor_specific_content(queue='short',
+        new_content = jw.condor_specific_content(queue='long',
                                                  machine='llrt3condor')
         contents.insert(ncontents-1, new_content + '\n')
         with open(out, 'w') as f:
