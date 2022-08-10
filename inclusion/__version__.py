99845b1e2b4063999ece299565558a250b857afa
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-08-10 20:41:03 +0200

    :bricks: add dummy version file

---
+++ inclusion/__version__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+"""
+Trigger scale factor framework.
+"""
+
+__author__    = 'Bruno Alves'
+__copyright__ = 'Copyright 2022, Bruno Alves'
+__credits__   = ['Bruno Alves']
+__contact__   = 'https://github.com/bfonta/inclusion'
+__license__   = 'MIT'
+__status__    = 'Development'
+__version__   = '0.0.1'
