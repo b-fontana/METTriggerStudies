1566507613dc90cfe958ac96188f79fd9e5e2861
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-09-16 15:56:05 +0200

    :sparkle: copy outputs to EOS www service

--- inclusion/copy_to_eos.sh
+++ inclusion/copy_to_eos.sh
@@ -10,9 +10,8 @@ function print_usage_workflowClean {
 where:
     -h / --help  [ ${help_description} ]
     -t / --tag   [ ${tag_description} ]
-    -f / --full  [ ${full_description} ]
-    --eos  [ ${eos_description} ]
     -d / --dryrun [ ${dryrun_description} ]
+    --eos  [ ${eos_description} ]
 
     Run example: bash $(basename "$0") -t 000 -t 111 -t 222 -t 654 -f -d
 "
@@ -20,7 +19,7 @@ where:
 }
 
 ### Argument parsing
-DRYRUN=true # safety during script development
+DRYRUN=0 # safety during script development
 EOS_USER="bfontana"
 declare -a TAGS;
 while [[ $# -gt 0 ]]; do
@@ -42,7 +41,7 @@ while [[ $# -gt 0 ]]; do
 	    shift # past value
 	    ;;
 	-d|--dryrun)
-	    DRYRUN=true
+	    DRYRUN=1
 	    shift # past argument
 	    ;;
 	*)    # unknown option
@@ -53,14 +52,16 @@ while [[ $# -gt 0 ]]; do
 done
 
 ### Load functions
-source "lib/funcs.sh"
+THIS_FILE="${BASH_SOURCE[0]}"
+THIS_DIR="$( cd "$( dirname ${THIS_FILE} )" && pwd )"
+source "${THIS_DIR}/lib/funcs.sh"
 
 ### General parameters
 BASE_LOCAL_DIR="/data_CMS/cms/${USER}/TriggerScaleFactors"
 BASE_EOS_DIR="/eos/user/${EOS_USER:0:1}/${EOS_USER}/www/TriggerScaleFactors"
 declare -a CHANNELS=( "etau" "mutau" "tautau" )
 
-declare -a OLD_TAGS=( $(/bin/ls -1 "${BASE_PATH}") )
+declare -a OLD_TAGS=( $(/bin/ls -1 "${BASE_LOCAL_DIR}") )
 check_tags ${#TAGS[@]} ${TAGS[@]} ${OLD_TAGS[@]}
 
 declare -a REMOVED_TAGS;
@@ -94,11 +95,13 @@ echo "EOS_USER  = ${EOS_USER}"
 echo "#############"
 
 
-[[ ! -d ${EOS_DIR} ]] && /opt/exp_soft/cms/t3/eos-login -username ${EOS_USER} -init
-
+[[ ! -d ${EOS_DIR} ]] || /opt/exp_soft/cms/t3/eos-login -username ${EOS_USER} -init
 for tag in ${TAGS[@]}; do
 	for chn in ${CHANNELS[@]}; do
-		comm="cp ${BASE_LOCAL_DIR}/${tag}/Outputs/${chn}/Counts_Data_MET_EG_Mu/table.csv ${BASE_EOS_DIR}/New2/table.csv"
-		[[ ${DRYRUN} -eq 1 ]] && echo ${comm} || ${comm}
+		comm="cp -r ${BASE_LOCAL_DIR}/${tag}/Outputs/${chn} ${BASE_EOS_DIR}/${tag}"
+		echo ${comm}
+		if [[ ${DRYRUN} -eq 0 ]]; then
+			${comm}
+		fi	
 	done
 done
