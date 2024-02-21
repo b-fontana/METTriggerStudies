4f3d1c04a2417054b423df7933f4d4a7bee66536
Bruno Alves <balves16@hotmail.com>
2024-02-21 22:35:20 +0100

    Fix x range.

--- tests/compare_ratios.py
+++ tests/compare_ratios.py
@@ -17,7 +17,6 @@ def build_path(base, channel, variable):
     path = os.path.join(base, channel, variable)
     return os.path.join(path, "eff_Data_Mu_MC_TT_DY_WJets_" + channel + "_" + variable + "_TRG_METNoMu120_CUTS_*.root")
 
-
 def sigmoid(x, params):
     """
     Sigmoid function to mimick the TF1 object.
@@ -40,17 +39,17 @@ def compare_ratios(tag, channels, variable, var_units):
             raise RuntimeError('[ERROR] Path {} must have lenght 1.'.format(tmp))
         paths.append(tmp[0])
 
-    mu = '\u03BC'
-    tau = '\u03C4'
-    dd = {"mumu": mu+mu,
-          "mutau": mu+tau}
+    mu, tau = '\u03BC','\u03C4'
+    dd = {"mumu": mu+mu, "mutau": mu+tau}
 
-    colors = ("blue", "green", "red", "brown")
-    labels = ("full", r"partial: $[180:\infty[$", r"partial: $[160:\infty[$", r"partial: $[150:\infty[$")
+    colors = ("blue", "green", "red", "purple")
+    line_opt = dict(color="grey", linestyle="--")
+    labels = ("full", r"$[180;\infty[\:\:{}$".format(var_units),
+              r"$[160;\infty[\:\:{}$".format(var_units), r"$[150;\infty[\:\:{}$".format(var_units))
     met_cuts = [180., 160., 150.]
     var_map = dict(metnomu_et=r"MET (no-$\mu$)")
 
-    ratios = []
+    ratios, idx_lims = [], []
     
     fig, (ax1, ax2) = plt.subplots(2, sharex=True, gridspec_kw={'height_ratios': [3., 1.]})
     plt.subplots_adjust(wspace=0, hspace=0)
@@ -61,44 +60,52 @@ def compare_ratios(tag, channels, variable, var_units):
         fit_data = up.open(bpath + ":SigmoidFuncData")
         fit_mc = up.open(bpath + ":SigmoidFuncMC")
 
-        fit_data_pars   = fit_data.member('fFormula').member('fClingParameters')[:]
-        fit_data_xrange = (fit_data.member('fXmin'), fit_data.member('fXmax'))
-        fit_mc_pars     = fit_mc.member('fFormula').member('fClingParameters')[:]
-        fit_mc_xrange   = (fit_mc.member('fXmin'), fit_mc.member('fXmax'))
-        assert fit_data_xrange == fit_mc_xrange
+        fit_data_pars = fit_data.member('fFormula').member('fClingParameters')[:]
+        fit_mc_pars   = fit_mc.member('fFormula').member('fClingParameters')[:]
+        fit_xrange    = (fit_data.member('fXmin'), fit_data.member('fXmax'))
+        assert fit_xrange == (fit_mc.member('fXmin'), fit_mc.member('fXmax'))
 
-        # fit_data_xvals = np.linspace(*fit_data_xrange, num=500)
-        # fit_mc_xvals   = np.linspace(*fit_mc_xrange, num=500)
         fit_xvals = np.linspace(0, 350, num=5000)
 
         fit_data_yvals = sigmoid(fit_xvals, fit_data_pars)
         fit_mc_yvals   = sigmoid(fit_xvals, fit_mc_pars)
-        breakpoint()
-        ratios.append(fit_data_yvals / fit_mc_yvals)
-        
-        ax1.plot(fit_data_xvals, ratios[-1], '--', color=colors[ipath])
+
+        # get fit validity range, otherwise the full function is plotted
+        idx_lims.append( (np.argmax(fit_xvals > fit_xrange[0]),
+                          np.argmax(fit_xvals > fit_xrange[1])) )
+        idx_sel = slice(idx_lims[-1][0],idx_lims[-1][1],1)
+
+        # plot efficiency values and error bars
         ax1.errorbar(graph.values(axis="x"), graph.values(axis="y"),
                      xerr=(graph.errors(axis="x", which="low"),graph.errors(axis="x", which="high")),
                      yerr=(graph.errors(axis="y", which="low"),graph.errors(axis="y", which="high")),
-                     fmt='o', color=colors[ipath], label=labels[ipath])
+                     fmt='o', color="black")
+
+        ratios.append(fit_data_yvals / fit_mc_yvals)
+
+        # plot SF fit (Data/MC)
+        ax1.plot(fit_xvals[idx_sel], ratios[-1][idx_sel], '--', color=colors[ipath], label=labels[ipath])
 
         if ipath != 0:
-            ax1.axvline(x=met_cuts[ipath-1], color="grey", linestyle="--")
-            ax1.axhline(y=1., color="grey", linestyle="--")
-            ax2.axvline(x=met_cuts[ipath-1], color="grey", linestyle="--")
+            ax1.axvline(x=met_cuts[ipath-1], **line_opt)
+            ax1.axhline(y=1., **line_opt)
+            ax2.axvline(x=met_cuts[ipath-1], **line_opt)
             
     ax1.set_ylabel("Data / MC", fontsize=20)
     ax1.legend(loc="lower right")
     ax1.set_ylim(0.64, 1.015)
-    for yval in (-0.05, 0., 0.05, 0.1):
-        ax2.axhline(y=yval, color="grey", linestyle="--")
-        ax2.set_ylim(-0.075, 0.145)
+    for yval in (0., 0.0025, 0.005):
+        ax2.axhline(y=yval, **line_opt)
+        ax2.set_ylim(-0.003, 0.007)
 
-    # comparison of ratio relative using the first partial fit as reference
+    # comparison of ratios using the first partial fit as reference
+    # the x range is the minimum interval common to both ratios
     for ipath, bpath in enumerate(paths[1:]):
-        ax2.plot(fit_data_xvals, (ratios[1]/ratios[ipath+1])-1., '--', color=colors[ipath+1])
+        tmp_sel = slice(max(idx_lims[1][0], idx_lims[ipath+1][0]),
+                        min(idx_lims[1][1], idx_lims[ipath+1][1]), 1)
+        ax2.plot(fit_xvals[tmp_sel], (ratios[1][tmp_sel]/ratios[ipath+1][tmp_sel])-1., '--', color=colors[ipath+1])
 
-    ax2.set_ylabel(r"$(SF_{{180}}/SF) - 1$", fontsize=20)
+    ax2.set_ylabel(r"$(SF_{{180\:{u}}}/SF_{{X\:{u}}}) - 1$".format(u=var_units), fontsize=20)
     ax2.set_xlabel(var_map[variable] + " [" + var_units + "]", fontsize=21)
     
     hep.cms.text(' Preliminary', fontsize=22, ax=ax1)
