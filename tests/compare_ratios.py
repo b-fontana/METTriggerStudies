a2f20ecec01744029d253e9d2521c0a6ef1fa1f7
Bruno Alves <balves16@hotmail.com>
2024-02-21 18:50:28 +0100

    Generalize to n curves.

--- tests/compare_ratios.py
+++ tests/compare_ratios.py
@@ -15,8 +15,8 @@ plt.style.use(hep.style.ROOT)
 
 def build_path(base, channel, variable):
     path = os.path.join(base, channel, variable)
-    return os.path.join(path, "eff_Data_Mu_MC_DY_TT_WJets_" + channel + "_" + variable + "_TRG_METNoMu120_CUTS_*.root")
-    #return os.path.join(path, "eff_Data_Mu_MC_TT_DY_" + channel + "_" + variable + "_TRG_METNoMu120_CUTS_*.root")
+    return os.path.join(path, "eff_Data_Mu_MC_TT_DY_WJets_" + channel + "_" + variable + "_TRG_METNoMu120_CUTS_*.root")
+
 
 def sigmoid(x, params):
     """
@@ -28,91 +28,87 @@ def sigmoid(x, params):
 def compare_ratios(tag, channels, variable, var_units):
     base = os.path.join("/data_CMS/cms/alves/TriggerScaleFactors/", tag, "Outputs")
 
-    bpath1 = build_path(base, channels[0], variable)
-    path1 = glob.glob(bpath1)
-    if len(path1) != 1:
-        print(bpath1)
-        print(path1)
-        raise RuntimeError('s[ERROR] Path must have lenght 1.')
-    bpath2 = build_path(base, channels[1], variable)
-    path2 = glob.glob(bpath2)
-    if len(path2) != 1:
-        print(bpath2)
-        print(path2)
-        raise RuntimeError('[ERROR] Path must have lenght 1.')
-    assert len(path2) == 1
+    #bpaths = build_path(base, channels[0], variable)
+    _paths = (build_path(base, channels[1], variable),
+              "180_mumu_fit.root", "160_mumu_fit.root", "150_mumu_fit.root")
+
+    paths = []
+    for bpath in _paths:
+        tmp = glob.glob(bpath)
+        if len(tmp) != 1:
+            print(tmp)
+            raise RuntimeError('[ERROR] Path {} must have lenght 1.'.format(tmp))
+        paths.append(tmp[0])
 
     mu = '\u03BC'
     tau = '\u03C4'
     dd = {"mumu": mu+mu,
           "mutau": mu+tau}
-    
-    g1 = up.open(path1[0] + ":SF1D")
-    g2 = up.open(path2[0] + ":SF1D")
-    fit1_data = up.open(path1[0] + ":SigmoidFuncData")
-    fit1_mc   = up.open(path1[0] + ":SigmoidFuncMC")
-    fit2_data = up.open(path2[0] + ":SigmoidFuncData")
-    fit2_mc   = up.open(path2[0] + ":SigmoidFuncMC")
 
-    fit1_data_pars   = fit1_data.member('fFormula').member('fClingParameters')[:]
-    fit1_data_xrange = (fit1_data.member('fXmin'), fit1_data.member('fXmax'))
-    fit1_mc_pars     = fit1_mc.member('fFormula').member('fClingParameters')[:]
-    fit1_mc_xrange   = (fit1_mc.member('fXmin'), fit1_mc.member('fXmax'))
-    assert fit1_data_xrange == fit1_mc_xrange
+    colors = ("blue", "green", "red", "brown")
+    labels = ("full", r"partial: $[180:\infty[$", r"partial: $[160:\infty[$", r"partial: $[150:\infty[$")
+    met_cuts = [180., 160., 150.]
+    var_map = dict(metnomu_et=r"MET (no-$\mu$)")
 
-    fit2_data_pars   = fit2_data.member('fFormula').member('fClingParameters')[:]
-    fit2_data_xrange = (fit2_data.member('fXmin'), fit2_data.member('fXmax'))
-    fit2_mc_pars     = fit2_mc.member('fFormula').member('fClingParameters')[:]
-    fit2_mc_xrange   = (fit2_mc.member('fXmin'), fit2_mc.member('fXmax'))
-    assert fit2_data_xrange == fit2_mc_xrange
-
-    fit1_data_xvals = np.linspace(*fit1_data_xrange, num=500)
-    fit1_data_yvals = sigmoid(fit1_data_xvals, fit1_data_pars)
-    fit1_mc_xvals   = np.linspace(*fit1_mc_xrange, num=500)
-    fit1_mc_yvals   = sigmoid(fit1_mc_xvals, fit1_mc_pars)
-
-    fit2_data_xvals = np.linspace(*fit2_data_xrange, num=500)
-    fit2_data_yvals = sigmoid(fit2_data_xvals, fit2_data_pars)
-    fit2_mc_xvals   = np.linspace(*fit2_mc_xrange, num=500)
-    fit2_mc_yvals   = sigmoid(fit2_mc_xvals, fit2_mc_pars)
-
-    ratio1 = fit1_data_yvals / fit1_mc_yvals
-    ratio2 = fit2_data_yvals / fit2_mc_yvals
-    met_cut = 180.
+    ratios = []
     
     fig, (ax1, ax2) = plt.subplots(2, sharex=True, gridspec_kw={'height_ratios': [3., 1.]})
     plt.subplots_adjust(wspace=0, hspace=0)
-    ax1.plot(fit1_data_xvals, ratio1, '--', color="blue")
-    ax1.errorbar(g1.values(axis="x"), g1.values(axis="y"),
-                 xerr=(g1.errors(axis="x", which="low"),g1.errors(axis="x", which="high")),
-                 yerr=(g1.errors(axis="y", which="low"),g1.errors(axis="y", which="high")),
-                 fmt='o', label=dd[channels[0]], color="blue")
 
-    ax1.plot(fit2_data_xvals, ratio2, '--', color="orange")
-    ax1.errorbar(g2.values(axis="x"), g2.values(axis="y"),
-                 xerr=(g2.errors(axis="x", which="low"),g2.errors(axis="x", which="high")),
-                 yerr=(g2.errors(axis="y", which="low"),g2.errors(axis="y", which="high")),
-                 fmt='o', label=dd[channels[1]], color="orange")
+    for ipath, bpath in enumerate(paths):
+        graph = up.open(bpath + ":SF1D")
+        
+        fit_data = up.open(bpath + ":SigmoidFuncData")
+        fit_mc = up.open(bpath + ":SigmoidFuncMC")
 
-    ax1.axvline(x=met_cut, color="grey", linestyle="--")
-    ax1.axhline(y=1., color="grey", linestyle="--")
+        fit_data_pars   = fit_data.member('fFormula').member('fClingParameters')[:]
+        fit_data_xrange = (fit_data.member('fXmin'), fit_data.member('fXmax'))
+        fit_mc_pars     = fit_mc.member('fFormula').member('fClingParameters')[:]
+        fit_mc_xrange   = (fit_mc.member('fXmin'), fit_mc.member('fXmax'))
+        assert fit_data_xrange == fit_mc_xrange
+
+        # fit_data_xvals = np.linspace(*fit_data_xrange, num=500)
+        # fit_mc_xvals   = np.linspace(*fit_mc_xrange, num=500)
+        fit_xvals = np.linspace(0, 350, num=5000)
+
+        fit_data_yvals = sigmoid(fit_xvals, fit_data_pars)
+        fit_mc_yvals   = sigmoid(fit_xvals, fit_mc_pars)
+        breakpoint()
+        ratios.append(fit_data_yvals / fit_mc_yvals)
+        
+        ax1.plot(fit_data_xvals, ratios[-1], '--', color=colors[ipath])
+        ax1.errorbar(graph.values(axis="x"), graph.values(axis="y"),
+                     xerr=(graph.errors(axis="x", which="low"),graph.errors(axis="x", which="high")),
+                     yerr=(graph.errors(axis="y", which="low"),graph.errors(axis="y", which="high")),
+                     fmt='o', color=colors[ipath], label=labels[ipath])
+
+        if ipath != 0:
+            ax1.axvline(x=met_cuts[ipath-1], color="grey", linestyle="--")
+            ax1.axhline(y=1., color="grey", linestyle="--")
+            ax2.axvline(x=met_cuts[ipath-1], color="grey", linestyle="--")
+            
     ax1.set_ylabel("Data / MC", fontsize=20)
     ax1.legend(loc="lower right")
-
-    ax2.axvline(x=met_cut, color="grey", linestyle="--")
-    for yval in (.95, 1., 1.05):
+    ax1.set_ylim(0.64, 1.015)
+    for yval in (-0.05, 0., 0.05, 0.1):
         ax2.axhline(y=yval, color="grey", linestyle="--")
-    ax2.set_ylim(0.905, 1.095)
-    ax2.plot(fit1_data_xvals, ratio1 / ratio2, '--', color="green")
-    ax2.set_ylabel("{} / {}".format(dd[channels[0]], dd[channels[1]]), fontsize=20)
-    ax2.set_xlabel(variable + " [" + var_units + "]", fontsize=21)
+        ax2.set_ylim(-0.075, 0.145)
+
+    # comparison of ratio relative using the first partial fit as reference
+    for ipath, bpath in enumerate(paths[1:]):
+        ax2.plot(fit_data_xvals, (ratios[1]/ratios[ipath+1])-1., '--', color=colors[ipath+1])
+
+    ax2.set_ylabel(r"$(SF_{{180}}/SF) - 1$", fontsize=20)
+    ax2.set_xlabel(var_map[variable] + " [" + var_units + "]", fontsize=21)
     
     hep.cms.text(' Preliminary', fontsize=22, ax=ax1)
     hep.cms.lumitext(r"59.7 $fb^{-1}$ (13 TeV)", fontsize=21, ax=ax1)
 
-    output = __file__[:-3] + '.png'
-    fig.savefig(output)
-    print('Plot save under {}'.format(output))
+    output = os.path.join("/eos/home-b/bfontana/www/TriggerScaleFactors/CompareRatios/",
+                          os.path.basename(__file__[:-3]))
+    for ext in ('.png', '.pdf'):
+        fig.savefig(output + ext)
+        print('Plot saved under {}'.format(output))
 
     
 if __name__ == "__main__":
