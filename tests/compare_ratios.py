8992e636372cca13a5b5a16ee5de04f81b7accfb
Bruno Alves <balves16@hotmail.com>
2024-03-19 09:06:59 +0100

    Support year comparison.

--- tests/compare_ratios.py
+++ tests/compare_ratios.py
@@ -20,14 +20,18 @@ def build_path(base, channel, variable):
     path = os.path.join(base, channel, variable)
     return os.path.join(path, "eff_Data_Mu_MC_TT_DY_WJets_" + channel + "_" + variable + "_TRG_METNoMu120_CUTS_*.root")
 
-def get_paths_and_labels(base, mode, channels, variable, var_units):   
+def get_paths_and_labels(base, mode, channels, variable, year, var_units):
     if mode == "ranges":
-        labels = ("full", r"$[180;\infty[\:\:{}$".format(var_units),
-                  r"$[160;\infty[\:\:{}$".format(var_units), r"$[150;\infty[\:\:{}$".format(var_units))
+        labels = ["full", r"$[180;\infty[\:\:{}$".format(var_units),
+                  r"$[160;\infty[\:\:{}$".format(var_units), r"$[150;\infty[\:\:{}$".format(var_units)]
+        if year == "2018":
+            labels.append(r"$[140;\infty[\:\:{}$".format(var_units))
         # transfer files with:
         # cp /data_CMS/cms/alves/TriggerScaleFactors/OpenCADI_18/Outputs/mutau/metnomu_et/eff_Data_Mu_MC_TT_DY_WJets_mutau_metnomu_et_TRG_METNoMu120_CUTS_mhtnomu_et_L_0p0_default.root full_mumu_fit.root
-        paths = ("full_mumu_fit.root",
-                 "180_mumu_fit.root", "160_mumu_fit.root", "150_mumu_fit.root")
+        paths = ["full_mumu_fit_"+year+".root", "180_mumu_fit_"+year+".root",
+                 "160_mumu_fit_"+year+".root", "150_mumu_fit_"+year+".root"]
+        if year == "2018":
+            paths.append("140_mumu_fit_"+year+".root")
     elif mode == "channels":
         labels = (dd["mutau"], dd["mumu"])
         paths = (build_path(base, channels[0], variable),
@@ -37,6 +41,9 @@ def get_paths_and_labels(base, mode, channels, variable, var_units):
                   dd["mutau"] + ", SingleMuon", dd["mutau"] + ", DoubleMuon")
         paths = ("full_mumu_fit.root", "full_double_mumu_fit.root",
                  "full_mutau_fit.root", "full_double_mutau_fit.root")
+    elif mode == "years":
+        labels = ("UL17", "UL18")
+        paths = ("160_mumu_fit_2017.root", "150_mumu_fit_2018.root")
 
     ret = {}
     for p,l in zip(paths,labels):
@@ -55,13 +62,13 @@ def sigmoid(x, params):
     """
     return params[2] / (1 + np.exp(-params[0] * (x - params[1])))
 
-def compare_ratios(paths, mode, variable, var_units):
+def compare_ratios(paths, mode, variable, year, var_units):
     """
     Compare ratios in two modes.
     - Mode 'ranges': compare change in fit from changing the fit range
     - Mode 'channels': compare changes from fitting different channels
     """
-    colors = ("blue", "green", "red", "purple")
+    colors = ("blue", "green", "red", "purple", "darkorange")
     var_map = dict(metnomu_et=r"MET-no$\mu$")
 
     fit_ratios, idx_lims = [], []
@@ -108,9 +115,11 @@ def compare_ratios(paths, mode, variable, var_units):
 
     line_opt = dict(color="grey", linestyle="--")
     met_cuts = [180., 160., 150.]
+    if year=="2018":
+        met_cuts.append(140.)
     if mode == "ranges":
         ax1.set_ylim(-0.05, 1.08)
-        yticks = np.arange(-.04, .04, .01)
+        yticks = np.arange(-.06, .06, .02)
         ax2.set_yticks(yticks)
         for yval in yticks:
             ax2.axhline(y=yval, **line_opt)
@@ -141,7 +150,18 @@ def compare_ratios(paths, mode, variable, var_units):
         for yval in yticks:
             ax2.axhline(y=yval, **line_opt)
         ax2.set_ylim(yticks[0]+1E-5, yticks[-1]-1E-5)
-        
+
+    elif mode == "years":
+        ax1.set_ylim(-0.05, 1.08)
+        ax1.axvline(x=150., **line_opt)
+        ax2.axvline(x=150., **line_opt)
+        yticks = (-.1, 0., .1)
+        ax2.set_yticks(yticks)
+        ax1.axhline(y=1., **line_opt)
+        for yval in yticks:
+            ax2.axhline(y=yval, **line_opt)
+        ax2.set_ylim(-.19, .19)
+
     # comparison of ratios using the first partial fit as reference
     # the x range is the minimum interval common to both ratios
     if mode == "ranges":
@@ -150,38 +170,49 @@ def compare_ratios(paths, mode, variable, var_units):
                 continue
             tmp_sel = slice(max(idx_lims[1][0], idx_lims[ipath][0]),
                             min(idx_lims[1][1], idx_lims[ipath][1]), 1)
-            ax2.plot(fit_xvals[tmp_sel], (fit_ratios[1][tmp_sel]/fit_ratios[ipath][tmp_sel])-1., '--', color=colors[ipath])
+            ax2.plot(fit_xvals[tmp_sel], (fit_ratios[3][tmp_sel]/fit_ratios[ipath][tmp_sel])-1., '--', color=colors[ipath])
     elif mode == "channels":
         ax2.plot(fit_xvals, (fit_ratios[1]/fit_ratios[0])-1., '--', color=colors[ipath])
-
     elif mode == "datasets":
         ax2.plot(fit_xvals, (fit_ratios[1]/fit_ratios[0])-1., '--', color="blue",
                  label=dd["mumu"] + ": Double/Single")
         ax2.plot(fit_xvals, (fit_ratios[3]/fit_ratios[2])-1., '--', color="orange",
                  label=dd["mutau"] + ": Double/Single")
+    elif mode == "years":
+        tmp_sel = slice(max(idx_lims[1][0], idx_lims[0][0]),
+                        min(idx_lims[1][1], idx_lims[0][1]), 1)
+        ax2.plot(fit_xvals[tmp_sel], (fit_ratios[1][tmp_sel]/fit_ratios[0][tmp_sel])-1., '--', color=colors[ipath])
 
     if mode == "ranges":
-        ax2.set_ylabel(r"$(SF_{{180\:{u}}}/SF_{{X\:{u}}}) - 1$".format(u=var_units), fontsize=20)
+        ax2.set_ylabel(r"$(SF_{{150\:{u}}}/SF_{{X\:{u}}}) - 1$".format(u=var_units), fontsize=20)
     elif mode == "channels":
         ax2.set_ylabel(r"Fit Ratio", fontsize=20)
     elif mode == "datasets":
         ax2.set_ylabel(r"Fit Ratio", fontsize=20)
+    elif mode == "years":
+        ax2.set_ylabel(r"UL18/UL17 - 1", fontsize=20)
     ax2.set_xlabel(var_map[variable] + " [" + var_units + "]", fontsize=21)
 
     if mode == "datasets":
         ax2.legend(loc="lower right", fontsize=15)
         
     hep_opt = dict(ax=ax1)
+    lumi = {"2016APV": "19.5", "2016": "16.8", "2017": "41.5", "2018": "59.7"}[year]
     hep.cms.text(' Preliminary', fontsize=22, **hep_opt)
     if mode == "ranges":
-        hep.cms.lumitext(dd["mumu"] + " (baseline, 2018) | " + r"59.7 $fb^{-1}$ (13 TeV)",
+        hep.cms.lumitext(dd["mumu"] + " (baseline, {}) | ".format(year) + r"{} $fb^{{-1}}$ (13 TeV)".format(lumi),
                          fontsize=19, **hep_opt)
+    elif mode == "years":
+        hep.cms.lumitext(r"41.7 $fb^{-1}$ [UL17], 59.7 $fb^{-1}$ [UL18] (13 TeV)", fontsize=19, **hep_opt)
     else:
-        hep.cms.lumitext(r"59.7 $fb^{-1}$ (13 TeV)",
-                         fontsize=19, **hep_opt)
+        hep.cms.lumitext(r"59.7 $fb^{-1}$ (13 TeV)", fontsize=19, **hep_opt)
 
-    output = os.path.join("/eos/home-b/bfontana/www/TriggerScaleFactors/CompareRatios/",
-                          os.path.basename(__file__[:-3] + '_' + mode))
+    baseout = "/eos/home-b/bfontana/www/TriggerScaleFactors/CompareRatios/"
+    if mode == "years":
+        output = os.path.join(baseout, os.path.basename(__file__[:-3] + '_' + mode))
+    else:
+        output = os.path.join(baseout, os.path.basename(__file__[:-3] + '_' + mode + '_' + year))
+        
     for ext in ('.png', '.pdf'):
         fig.savefig(output + ext)
         print('Plot saved under {}'.format(output + ext))
@@ -191,8 +222,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Compare efficiency ratios obtained with two different methods.')
     parser.add_argument('--tag', required=False, default="",
                         help='Tag used to produce the graphs. Same used by the inclusion/run.py command.')
-    parser.add_argument('--mode', default="channel", choices=("ranges", "channels", "datasets"),
+    parser.add_argument('--mode', default="channel", choices=("ranges", "channels", "datasets", "years"),
                         help='Which comparison to run.')
+    parser.add_argument('--year', default="2018", choices=("2016APV", "2016", "2017", "2018"),
+                        help='Which data period to consider.')
     
     FLAGS = parser.parse_args()
     if FLAGS.mode == "channels":
@@ -200,5 +233,5 @@ if __name__ == "__main__":
     
     base = os.path.join("/data_CMS/cms/alves/TriggerScaleFactors/", FLAGS.tag, "Outputs")
     paths = get_paths_and_labels(base, FLAGS.mode, channels=("mutau", "mumu"),
-                                 variable="metnomu_et", var_units="GeV")
-    compare_ratios(paths, mode=FLAGS.mode, variable="metnomu_et", var_units="GeV")
+                                 variable="metnomu_et", year=FLAGS.year, var_units="GeV")
+    compare_ratios(paths, mode=FLAGS.mode, variable="metnomu_et", year=FLAGS.year, var_units="GeV")
